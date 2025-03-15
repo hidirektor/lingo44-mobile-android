9a48d02eef90ee31ccdc1fc6f1341b4be8ef5f4a
hidirektor <hidirektor@gmail.com>
2025-03-15 03:03:13 +0300

    Removed Debug on Theme Changing
    Signed-off-by: hidirektor <hidirektor@gmail.com>

--- app/src/main/java/com/hidirektor/lingify/UI/Screens/Welcome/WelcomeActivity.java
+++ app/src/main/java/com/hidirektor/lingify/UI/Screens/Welcome/WelcomeActivity.java
@@ -1,15 +1,10 @@
 package com.hidirektor.lingify.UI.Screens.Welcome;
 
-import android.content.res.Configuration;
-import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.content.ContextCompat;
 
 import com.hidirektor.lingify.R;
 import com.hidirektor.lingify.Utility.Preferences.Theme.ThemeUtil;
@@ -18,8 +13,6 @@ import com.hidirektor.lingify.Utility.SystemDefaults;
 public class WelcomeActivity extends AppCompatActivity {
 
     private ImageView themeChangerButton;
-    private ImageView appLogo;
-    private ImageView welcomeStockImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +31,6 @@ public class WelcomeActivity extends AppCompatActivity {
 
     private void componentInitialize() {
         themeChangerButton = findViewById(R.id.themeChangerButton);
-        appLogo = findViewById(R.id.appLogo);
-        welcomeStockImage = findViewById(R.id.welcomeStockImage);
     }
 
     private void toggleTheme() {
@@ -50,20 +41,5 @@ public class WelcomeActivity extends AppCompatActivity {
 
         // Tema değiştir
         ThemeUtil.setTheme(WelcomeActivity.this, newMode);
-        updateDrawablesBasedOnTheme();
-    }
-
-    private void updateDrawablesBasedOnTheme() {
-        if (getResources().getConfiguration().uiMode == Configuration.UI_MODE_NIGHT_YES) {
-            appLogo.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.logo_kusadasi_dil_dark));
-            welcomeStockImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.image_welcome_dark));
-            themeChangerButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_sun));
-            Log.d("TEST213", "DARK");
-        } else {
-            appLogo.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.logo_kusadasi_dil_white));
-            welcomeStockImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.image_welcome_white));
-            themeChangerButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.icon_moon));
-            Log.d("TEST213", "LIGHT");
-        }
     }
 }
