32f235baca1195c41ac2ab7a5753d7004a6c21c7
hidirektor <hidirektor@gmail.com>
2025-03-16 21:49:27 +0300

    Detect Level Default Buttons
    Signed-off-by: hidirektor <hidirektor@gmail.com>

--- app/src/main/java/com/hidirektor/lingify/UI/Screens/Setup/LanguageLevel/DetectSelectionActivity.java
+++ app/src/main/java/com/hidirektor/lingify/UI/Screens/Setup/LanguageLevel/DetectSelectionActivity.java
@@ -1,12 +1,20 @@
 package com.hidirektor.lingify.UI.Screens.Setup.LanguageLevel;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hidirektor.lingify.R;
+import com.hidirektor.lingify.Utility.Preferences.Theme.ThemeUtil;
 
 public class DetectSelectionActivity extends AppCompatActivity {
+
+    private ImageView themeChangerButton;
+    private Button pickLevelButton;
+    private LinearLayout startExamButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +22,15 @@ public class DetectSelectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_level_detect_selection);
 
         componentInitialize();
+
+        themeChangerButton.setOnClickListener(v -> {
+            ThemeUtil.changeTheme(DetectSelectionActivity.this);
+        });
     }
 
     private void componentInitialize() {
-
+        themeChangerButton = findViewById(R.id.themeChangerButton);
+        pickLevelButton = findViewById(R.id.pickLevelButton);
+        startExamButton = findViewById(R.id.startExamButton);
     }
 }
