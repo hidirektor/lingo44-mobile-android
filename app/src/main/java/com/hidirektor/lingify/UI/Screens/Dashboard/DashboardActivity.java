e4c221cbd7abf396677d4a48b1fbe442668b94fa
hidirektor <hidirektor@gmail.com>
2025-03-20 04:11:25 +0300

    Dashboard Update
    Signed-off-by: hidirektor <hidirektor@gmail.com>

--- app/src/main/java/com/hidirektor/lingify/UI/Screens/Dashboard/DashboardActivity.java
+++ app/src/main/java/com/hidirektor/lingify/UI/Screens/Dashboard/DashboardActivity.java
@@ -2,10 +2,19 @@ package com.hidirektor.lingify.UI.Screens.Dashboard;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.hidirektor.lingify.BaseActivity;
 import com.hidirektor.lingify.R;
 
 public class DashboardActivity extends BaseActivity {
+
+    private ConstraintLayout homeButton;
+    private ConstraintLayout podcastButton;
+    private FloatingActionButton dictionaryButton;
+    private ConstraintLayout practicesButton;
+    private ConstraintLayout statsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,10 @@ public class DashboardActivity extends BaseActivity {
     }
 
     private void componentInitialize() {
-
+        homeButton = findViewById(R.id.homeButton);
+        podcastButton = findViewById(R.id.podcastButton);
+        dictionaryButton = findViewById(R.id.dictionaryButton);
+        practicesButton = findViewById(R.id.practicesButton);
+        statsButton = findViewById(R.id.statsButton);
     }
 }
