ff2daf0ccfdc5768efccace8d5b0fc7e7fdb6277
hidirektor <hidirektor@gmail.com>
2025-06-22 22:58:41 +0300

    Activity Layout & Fonts Added
    Signed-off-by: hidirektor <hidirektor@gmail.com>

---
+++ app/src/main/java/com/hidirektor/lingo44/UI/Screens/Dictionary/DictionaryActivity.java
@@ -0,0 +1,22 @@
+package com.hidirektor.lingo44.UI.Screens.Dictionary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hidirektor.lingo44.R;
+
+public class DictionaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary);
+
+        componentInitialize();
+    }
+
+    private void componentInitialize() {
+
+    }
+}
